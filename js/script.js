e04e648e6d60d21dada06bcb8945b68b1ce8c5d3
Oleg Chaika <grozmaster@ukr.net>
2022-08-06 00:06:48 +0300

    add order__popup

--- js/script.js
+++ js/script.js
@@ -1,9 +1,22 @@
 $('.mobile__menu').on('click', function () {
     // $('.popup').addClass('active');
     $('.popup').slideDown('active');
-
+    
 });
 $('.close__popup').on('click', function () {
     // $('.popup').removeClass('active');
     $('.popup').slideUp('active');
 });
+
+
+$('.order__btn').on('click', function () {
+    // $('.order__popup').addClass('open');
+    $('.order__popup').slideDown('active');
+    
+});
+
+$('.order__popup-close').on('click', function () {
+    // $('.popup').removeClass('active');
+    $('.order__popup').slideUp('active');
+});
+
