795578f9457d60a7c5cdcc9be325e474e07f7228
Oleg Chaika <grozmaster@ukr.net>
2022-08-05 16:58:22 +0300

    js file created

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$('.mobile__menu').on('click', function () {
+    // $('.popup').addClass('active');
+    $('.popup').slideDown('active');
+
+});
+$('.close__popup').on('click', function () {
+    // $('.popup').removeClass('active');
+    $('.popup').slideUp('active');
+});
