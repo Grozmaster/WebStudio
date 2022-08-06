7aa1e086498261851313cbe1cd48a65717e94a2a
Oleg Chaika <grozmaster@ukr.net>
2022-08-06 19:27:21 +0300

    js modal created

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,29 @@
+//
+// (() => {
+//     const refs = {
+//       openModalBtn: document.querySelector("[data-modal-open]"),
+//       closeModalBtn: document.querySelector("[data-modal-close]"),
+//       modal: document.querySelector("[data-modal]"),
+//     };
+//
+//     refs.openModalBtn.addEventListener("click", toggleModal);
+//     refs.closeModalBtn.addEventListener("click", toggleModal);
+//
+//     function toggleModal() {
+//       refs.modal.classList.toggle("is-hidden");
+//     }
+//
+//
+//
+//   })();
 
-(() => {
-    const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-    }
+const openBtn = document.querySelector(".order__btn");
+const closeBtn = document.querySelector(".order__popup-close");
+const modalWindow = document.querySelector(".order__popup");
 
-    
+function toogleModal() {
+    modalWindow.classList.toggle("active");
+}
 
-  })();
+openBtn.addEventListener("click", toogleModal);
+closeBtn.addEventListener("click", toogleModal);
