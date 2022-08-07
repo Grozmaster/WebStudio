9d02b1410551d136252018e864446e55a3264b3d
Oleg Chaika <grozmaster@ukr.net>
2022-08-07 16:56:56 +0300

    js mobile menu created

--- js/modal.js
+++ js/modal.js
@@ -17,29 +17,36 @@
 //
 //   })();
 
-const openBtn = document.querySelector(".order__btn");
-const closeBtn = document.querySelector(".order__popup-close");
-const modal = document.querySelector(".order__popup");
+const openBtn = document.querySelector(".order__btn"); //класс кнопки открытия окна
+const closeBtn = document.querySelector(".order__popup-close");//класс кнопки закрытия окна
+const modal = document.querySelector(".order__popup");// класс модального окна
 
 openBtn.addEventListener('click',toggleModal);
 closeBtn.addEventListener('click',toggleModal);
 
-function toggleModal(){
+function toggleModal(){                          //функция для изменения класса окна,добавляет или удаляет второй класс 'active'
     modal.classList.toggle("active");
 }
 
-window.addEventListener('click', e => {
+window.addEventListener('click', e => { //последний клик по экрану браузера
     const target = e.target
 
-    console.log(modal.className)
-
-    if (!target.closest('.order__popup') && !target.closest('.order__btn') ) {
+    if (!target.closest('.order__popup') && !target.closest('.order__btn') ) { //закрыть окно при клике за пределами окна
         modal.classList.remove('active')
     }
-    console.log(modal.className)
 
 });
 
 
+const openMenu = document.querySelector('.mobile__menu');
+const closeMenu = document.querySelector('.close__popup');
+const modalMenu = document.querySelector('.popup');
 
+openMenu.addEventListener('click',toggleMenu);
+closeMenu.addEventListener('click',toggleMenu);
+
+
+function toggleMenu(){
+    modalMenu.classList.toggle('active');
+}
 
